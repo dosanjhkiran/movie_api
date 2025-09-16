bc8cf0630e76496668dce1ac9503b227868f2090
dosanjhkiran <115619247+dosanjhkiran@users.noreply.github.com>
2025-09-16 20:44:00 +1000

    packages

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // index.js
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+
 
 // ---- Middleware ----
 app.use(express.json());
